bfdf6d51e67af683270177f591e99a76375ce3fe
mehmonashah711-ops <mehmonashah711@gmail.com>
2026-09-15 20:38:11 +0500

    Create rectangle-area.cpp
    learning everyday something new

---
+++ rectangle-area.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float length;
+	float width;
+	float area;
+	cout << "enter length:";
+	cin >> length;
+	cout << "enter width:";
+	cin >> width;
+	area = length * width;
+	cout << "area=" << area;
+	return 0;
+}
